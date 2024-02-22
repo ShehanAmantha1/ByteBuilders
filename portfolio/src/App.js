5dc5428458f49159072a745a63540846fba070e1
janithra moramudali <janithramoramudali@gmail.com>
2024-02-22 20:23:18 +0530

    edit home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,7 @@ import './App.css';
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './Nav';
+import NavBar from './Components/Nav';
 import HomePage from 'C:/Users/janit/source/repos/ByteBuilders/ByteBuilders/portfolio/src/Components/Home.js';
 
 
--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,19 +1,28 @@
 // HomePage.js
 import React from 'react';
-
+import "C:/Users/janit/source/repos/ByteBuilders/ByteBuilders/portfolio/src/StyleSheet.css"
 function HomePage() {
-
-
   return (
-    <div>
-     
-
-      <div>
-        <h1>Welcome to the Home Page!</h1>
-        {/* Add your home page content here */}
+    <div className="homepage-container">
+      <div className="text-container">
+        <h2>Hello I'm Thanushka Navod</h2>
+        <h1>Photographer</h1>
+        <h2>Currently working for Kapture Studio</h2>
+        <div className="social-media-icons">
+          <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><img src="/icons8-facebook-50.png" alt="Facebook icon" /></a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src="/icons8-instagram-50.png" alt="Instagram icon" /></a>
+          {/* Add more social media icons as needed */}
+        </div>
+      </div>
+      <div className="image-container">
+        {/* Image */}
+        <img src="/426495649_1790483054760524_9089709248450113505_n.jpg" alt="Description of the content in the image" width="700" height="700" />
+        {/* Social media icons */}
+        
       </div>
     </div>
   );
 }
 
 export default HomePage;
+
