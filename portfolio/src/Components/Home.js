cc8b64bd76889db08173c9054f5713ca864fd4e9
janithra moramudali <janithramoramudali@gmail.com>
2024-02-24 18:43:05 +0530

    hello

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -12,21 +12,55 @@ function HomePage() {
           <h1>Photographer</h1>
           <h2>Currently working for Kapture Studio</h2>
           <div className="social-media-icons">
-            <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><img src="/icons8-facebook-50.png" alt="Facebook icon" /></a>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><img src="/icons8-instagram-50.png" alt="Instagram icon" /></a>
-            {/* Add more social media icons as needed */}
+            <div>
+            <div className="social-media-icon" data-name="Facebook">
+              <a href="https://www.facebook.com/Thulshika?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+              <span>Facebook</span>
+
+                <img src="/icons8-facebook-50.png" alt="Facebook icon" />
+              </a>
+            </div>
+            </div>
+<div>
+            <div className="social-media-icon" data-name="Instagram">
+              <a href="https://www.instagram.com/thul_99_?igsh=dzR0Nm9mcTZyOWZm" target="_blank" rel="noopener noreferrer">
+              <span>Instagram</span>
+
+                <img src="/icons8-instagram-50.png" alt="Instagram icon" />
+              </a>
+            </div>
+            </div>
+<div>
+            <div className="social-media-icon" data-name="Tiktok">
+              <a href="https://www.tiktok.com/@photo_gallery__thulshika?_t=8k6MKbfwMlx&_r=1" target="_blank" rel="noopener noreferrer">
+              <span>Tiktok</span>
+
+                <img src="/icons8-tiktok-50.png" alt="Tiktok icon" />
+              </a>
+
+            </div>
+            <div>
+            <div>
+            <img src="/Screenshot 2024-02-24 181451.png" alt="Tiktok icon" />
+
+            </div>
+
+            </div>
+
+            </div>
+
           </div>
         </div>
+
         <div className="image-container">
           {/* Image */}
           <img src="/WhatsApp Image 2024-02-24 at 11.41.05_24a44ceb.jpg" alt="Description of the content in the image" width="600" height="600" />
-          {/* Social media icons */}
         </div>
       </div>
 
       {/* Add the About component below the homepage content */}
-      <br/>
-      <br/>
+      <br />
+      <br />
       <div>
         <About />
       </div>
